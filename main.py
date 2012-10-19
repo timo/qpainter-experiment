05d98d70cdcce0d7b8d299564000c20f9bf2af81
Timo Paulssen <timonator@perpetuum-immobile.de>
2012-10-20 01:58:15 +0200

    now the current line will be highlighted in the image

--- main.py
+++ main.py
@@ -35,6 +35,8 @@ class MainWindow(kdeui.KMainWindow):
         self.last_correct_code = self.last_working_code
 
         self.animation_time = 0
+        self.cursor_line = 0
+        self.inside_highlight = False
 
         self.setup_ui()
 
@@ -60,15 +62,16 @@ class MainWindow(kdeui.KMainWindow):
         layout.addWidget(self.canvas)
 
         self.kate.textChanged.connect(self.onTextChanged)
-        print(dir(self.kate.markInterface()))
-        self.kate.markInterface().markToolTipRequested.connect(self.onMarkToolTip)
+        self.kate.activeView().cursorPositionChanged.connect(self.onCursorPosChanged)
+        #print("\n".join(dir(self.kate.activeView())))
+        #self.kate.markInterface().markToolTipRequested.connect(self.onMarkToolTip)
 
         cw.setLayout(layout)
         self.setCentralWidget(cw)
 
     def onTextChanged(self):
         #print(self.kate.text())
-        #print(self.kate.activeView().cursorPosition().line())
+        self.cursor_line = self.kate.activeView().cursorPosition().line()
 
         try:
             ast = compile(self.kate.text(), "<editor>", "exec")
@@ -77,6 +80,9 @@ class MainWindow(kdeui.KMainWindow):
         except SyntaxError:
             print("there were syntax errors")
             self.markup_errors()
+
+    def onCursorPosChanged(self):
+        self.cursor_line = self.kate.activeView().cursorPosition().line()
 
     def onMarkToolTip(self, doc, mark, position):
         QToolTip.showText(position, "<p style='white-space:pre'>%s</p>" % (self.current_error), self.kate.widget())
@@ -88,6 +94,7 @@ class MainWindow(kdeui.KMainWindow):
         mi.clearMarks()
 
         self.current_error = sys.exc_info()[1]
+        print(self.current_error)
 
         for tb in tbs:
             if tb[0] != "<editor>":
@@ -100,6 +107,29 @@ class MainWindow(kdeui.KMainWindow):
         mi = self.kate.markInterface()
         mi.clearMarks()
 
+    def exectracer(self, frame, event, arg):
+        if frame.f_code.co_filename != "<editor>":
+            return
+
+        if event == "line":
+            painter = frame.f_locals["p"]
+            if frame.f_lineno == self.cursor_line + 1:
+                painter.save()
+                pen = QPen(painter.pen())
+                pen.setColor(QColor.fromHsl(math.fmod(frame.f_locals["t"] * 60, 360), 150, 150))
+                painter.setPen(pen)
+                self.inside_highlight = True
+            elif frame.f_lineno > self.cursor_line + 1 and self.inside_highlight:
+                painter.restore()
+                self.inside_highlight = False
+        elif event == "return":
+            if self.inside_highlight:
+                painter = frame.f_locals["p"]
+                painter.restore()
+                self.inside_highlight = False
+        elif event == "call":
+            return self.exectracer
+
     def timerEvent(self, event):
         self.animation_time += 0.02
         self.image = QPixmap(canvas_size)
@@ -108,8 +138,12 @@ class MainWindow(kdeui.KMainWindow):
         globaldict = dict()
         globaldict.update(PyQt4.QtCore.__dict__)
         globaldict.update(PyQt4.QtGui.__dict__)
+        globaldict.update(math.__dict__)
+
+        sys.settrace(self.exectracer)
+
         try:
-            exec(self.last_correct_code, dict(t=self.animation_time, p=painter, m=math), globaldict)
+            exec(self.last_correct_code, globaldict, dict(t=self.animation_time, p=painter))
             self.last_working_code = self.last_correct_code
             self.clear_errors()
         except:
@@ -119,7 +153,7 @@ class MainWindow(kdeui.KMainWindow):
             painter = QPainter(self.image)
             self.last_working_code = self.last_working_code
             self.markup_errors()
-            exec(self.last_working_code, dict(t=self.animation_time, p=painter, m=math), globaldict)
+            exec(self.last_working_code, globaldict, dict(t=self.animation_time, p=painter))
         del painter
         self.canvas.setPixmap(self.image)
 
